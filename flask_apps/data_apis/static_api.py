6ff373038b35ac7b8f73d4a5300eea87fade1528
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-03-03 01:09:10 -0500

    updated static api with flask_restful

--- flask_apps/data_apis/static_api.py
+++ flask_apps/data_apis/static_api.py
@@ -1,10 +1,14 @@
 from flask import Flask, send_file
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def index():
-    return send_file('data/census_2010_ky.json')
+class Data(Resource):
+    def get(self):
+        return send_file('data/census_2010_ky.json')
+
+api.add_resource(Data, '/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
